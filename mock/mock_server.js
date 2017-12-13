57a32b8e5f365392f063a15a41a684dba75268e3
Grace <grace.scharf@gmail.com>
2017-12-12 21:38:57 -0500

    getting express server working

--- mock/mock_server.js
+++ mock/mock_server.js
@@ -1,18 +1,14 @@
-// const express = require("express")
-// const app = express()
+const express = require("express")
+const app = express()
 const bodyParser = require("body-parser")
 const bands = require("./bands.json")
 
-// app.use(morgan('dev'))
-// app.use(cors())
-// app.use(bodyParser.json())
-
-
-// app.get("/api/bands", (req, res) => (res.send(bands)))
-
-// app.listen(process.env.PORT || 3000)
-
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/api/bands", (req, res) => res.send('bands'))
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
