54ccbc9424838f5061b25c7c64e921facc2836cc
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2018-01-31 12:33:00 -0500

    add getGenreIDs endpoint and DB query, set up environmental vars

--- data/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    localURL: "mysql://root:carrot@127.0.0.1:3306/mtl_artist_database"
-}
--- server/real_server.js
+++ server/real_server.js
@@ -1,21 +1,20 @@
 const express = require("express")
 const app = express()
+require('dotenv').config();
 const bodyParser = require("body-parser")
 const bands = require("../data/new-bands.json")
 const morgan = require("morgan")
 const cors = require("cors")
 const mysql = require('promise-mysql')
-const {localURL} = require("../data/config")
-const databaseUrl = process.env.CLEARDB_DATABASE_URL || localURL
-const journalDataLoader = require("../lib/dataLoader.js")
+//the first environment variable is local to Heroku, the second is in a local .env file for development
+const databaseUrl = process.env.CLEARDB_DATABASE_URL || process.env.LOCAL_DATABASE_URL
+const dataLoader = require("../lib/dataLoader.js")
+console.log("database URL is", databaseUrl,". connecting now")
 
-console.log("database URL is", databaseUrl)
-
-const connection = mysql.createConnection(
-
-    databaseUrl
-)
-    .then(connection => InitializeApp(new journalDataLoader(connection)))
+//this next part first makes the connection to the database, then instantiates the dataLoader with
+//that connection, then finally initialises the Express server.
+const connection = mysql.createConnection(databaseUrl)
+    .then(connection => InitializeApp(new dataLoader(connection)))
 
 function InitializeApp(dataLoader) {
     console.log("starting server.")
@@ -27,12 +26,15 @@ function InitializeApp(dataLoader) {
     app.get('/', (req, res) => res.send("Bashu's Server is running!"))
 
 
-    
+    app.get("/api/v1/genreIDs", (req,res)=>{
+        console.log("band_IDs for ",req.query.genre," requested. Fetching..");
+        dataLoader.getIDs(req.query.genre).then(bandIdArray=>res.status(200).json(bandIdArray))
+    })
+
     app.get("/api/v1/bands/", (req, res) => {
-        console.log("received 12-band request. IDs are", req.query)
-        console.log("string is",req.query.bandIdArray)
+        console.log("received request for bands, number is", req.query.bandIdArray.length)
         dataLoader.getBands(req.query.bandIdArray).then(bands => {
-            console.log("we found bands:", bands);
+            console.log("we found bands");
 
             return res.status(200).json(bands)
         })
@@ -42,26 +44,9 @@ function InitializeApp(dataLoader) {
 
     //sends back one artist object
     app.get("/api/band/:id", (req, res) => {
-        //add a database call here
-
-        return res.status(200).json({
-            "Name": "Sweat",
-            "Genre": "Jangle Pop",
-            "Music Link": "http://sweatmtl.bandcamp.com/",
-            "Apple Music": "https://itunes.apple.com/ca/artist/sweat/1108674748",
-            "Spotify": "https://open.spotify.com/artist/3GqfGuJW6DWAwJxh8VwBA3",
-            "Facebook": "https://www.facebook.com/sosweatyband/?fref=ts",
-            "Instagram": "https://www.instagram.com/_sweatband/",
-            "Twitter": "N/A",
-            "YouTube": "N/A",
-            "Photos": "https://imgur.com/a/JBU1L",
-            "Band E-mail": "sweatbandmtl@gmail.com ",
-            "Managment E-Mail": "N/A",
-            "Booking E-Mail": "N/A",
-            "PR E-mail": "N/A",
-            "Band Members": "Robben Lent\nGeorgia Gleason\nJordan Siemens\nAlan McTavish"
-        })
+        
+        
     })
 
-    app.listen(process.env.PORT || 3000, () => console.log('Server is live!'))
+    app.listen(process.env.PORT || 3005, () => console.log('Server is live!'))
 }
