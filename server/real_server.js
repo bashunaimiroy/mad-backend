314e2239440cb3cb4a1a531b3e9c02045f4b7f29
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2018-03-01 12:34:59 -0500

    add user submission of bands
    server now has endpoint /api/v1/submit accepting POST requests.
    dataLoader now has addBand method

--- server/real_server.js
+++ server/real_server.js
@@ -9,19 +9,22 @@ const mysql = require('promise-mysql')
 //the first environment variable is local to Heroku, the second is in a local .env file for development
 const databaseUrl = process.env.CLEARDB_DATABASE_URL || process.env.LOCAL_DATABASE_URL
 const dataLoader = require("../lib/dataLoader.js")
+
 console.log("database URL is", databaseUrl, ". connecting now")
 
-//this next part first makes the connection to the database, then instantiates the dataLoader with
-//that connection, then finally initialises the Express server.
+//this next part first makes the connection to the database, 
+//then instantiates the dataLoader with that connection,
+// then finally initialises the Express server.
 const connection = mysql.createConnection(databaseUrl)
-    .then(connection => InitializeApp(new dataLoader(connection)))
+    .then(connection => initializeApp(new dataLoader(connection)))
 
-function InitializeApp(dataLoader) {
+function initializeApp(dataLoader) {
     console.log("starting server.")
     app.use(helmet())
     app.use(morgan('dev'))
     app.use(cors())
     app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
 
     app.get('/', (req, res) => res.send("You have reached the Montreal Artist Database back-end. Try an endpoint!"))
 
@@ -51,27 +54,37 @@ function InitializeApp(dataLoader) {
         })
     })
 
-        app.get("/api/v1/bands/", (req, res) => {
-            console.log("received request for bands, number is", req.query.bandIdArray.length)
-            dataLoader.getBands(req.query.bandIdArray).then(bands => {
-                console.log("we found bands");
+    app.get("/api/v1/bands/", (req, res) => {
+        console.log("received request for bands, number is", req.query.bandIdArray.length)
+        dataLoader.getBands(req.query.bandIdArray).then(bands => {
+            console.log("we found bands");
 
-                return res.status(200).json(bands)
-            }).catch(err => {
-                console.log(`error retrieving bands: 
+            return res.status(200).json(bands)
+        }).catch(err => {
+            console.log(`error retrieving bands: 
                         ${err.code} ${err.sqlMessage} 
                         SQL query was ${err.sql}`)
-                return res.status(500).json("Error retrieving Bands. Sorry")
+            return res.status(500).json("Error retrieving Bands. Sorry")
 
-            })
         })
+    })
 
-        //this is our error Handler Middleware
-        app.use(function (err, req, res, next) {
-            console.log(err)
-            res.status(500).send("We're sorry, we couldn't handle your request. Please contact the webmaster.")
-          })
-    
+    app.post("/api/v1/submit/", (req, res) => {
+        console.log("received submission for new band. name is", req.body.band_name)
+        dataLoader.addBand(req.body)
+            .then(id => {
+                console.log("band inserted. ID is ", id);
+                res.status(200).json({insertedId:id})
+            })
+            .catch(err => { console.error(err); res.status(500).send("Band submission failed. Please contact the webmaster.") })
+    })
 
-        app.listen(process.env.PORT || 3005, () => console.log('Server is live!'))
-    }
+    //this is our error Handler Middleware
+    app.use(function (err, req, res, next) {
+        console.log(err)
+        res.status(500).send("We're sorry, we couldn't handle your request. Please contact the webmaster.")
+    })
+
+
+    app.listen(process.env.PORT || 3005, () => console.log('Server is live!'))
+}
